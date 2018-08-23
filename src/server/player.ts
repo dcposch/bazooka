6ef344081dc00de50e62930e92d02216b63d7361
Eugene Chen <eugenechen@alum.mit.edu>
2018-08-23 12:39:17 -0700

    typescript server/player.ts

--- src/server/player.ts
+++ src/server/player.ts
@@ -1,10 +1,26 @@
-module.exports = Player
+import { PointXYZ } from '../types'
 
-function Player () {
-  this.name = 'unknown'
-  this.location = { x: 0, y: 0, z: 0 }
-  this.direction = { azimuth: 0, altitude: 0 }
-  this.velocity = { x: 0, y: 0, z: 0 }
-  this.situation = 'airborne'
-  this.health = 100
+interface Direction {
+  azimuth: number
+  altitude: number
 }
+
+class Player {
+  name: string
+  location: PointXYZ
+  direction: Direction
+  velocity: PointXYZ
+  situation: string
+  health: number
+
+  constructor() {
+    this.name = 'unknown'
+    this.location = { x: 0, y: 0, z: 0 }
+    this.direction = { azimuth: 0, altitude: 0 }
+    this.velocity = { x: 0, y: 0, z: 0 }
+    this.situation = 'airborne'
+    this.health = 100
+  }
+}
+
+export default Player
