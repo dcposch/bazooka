0a4d35b7d44f555ca6e6c964b5def4586715eb7a
Eugene Chen <eugenechen@alum.mit.edu>
2018-08-23 18:24:29 -0700

    typescript flex-buffer.ts

--- src/protocol/flex-buffer.ts
+++ src/protocol/flex-buffer.ts
@@ -1,48 +1,52 @@
-import nextPow2 from '../math/bit'.nextPow2
-
-module.exports = FlexBuffer
+import nextPow2 from '../math/bit'
 
 // A buffer that resizes automatically.
 // You can truncate it, append to it, then use slice() to get a Buffer.
-function FlexBuffer () {
-  this.buf = Buffer.alloc(1 << 20)
-  this.n = 0
-}
-
-FlexBuffer.prototype.reset = function () {
-  this.n = 0
-}
-
-FlexBuffer.prototype.writeInt32LE = function (v) {
-  resize(this, 4)
-  this.buf.writeInt32LE(v, this.n)
-  this.n += 4
-}
-
-FlexBuffer.prototype.writeUint8 = function (v) {
-  resize(this, 1)
-  this.buf.writeUint8(v, this.n)
-  this.n++
-}
-
-FlexBuffer.prototype.writeUint8Array = function (arr, start, len) {
-  if (start) {
-    var arrayBuf = arr.buffer // get the underlying ArrayBuffer
-    arr = new Uint8Array(arrayBuf.slice(start, len))
+class FlexBuffer {
+  buf: Buffer
+  n: number
+  constructor() {
+    this.buf = Buffer.alloc(1 << 20)
+    this.n = 0
   }
-  resize(this, arr.length)
-  this.buf.set(arr, this.n)
-  this.n += len
+
+  reset() {
+    this.n = 0
+  }
+
+  writeInt32LE(v: number) {
+    this.resize(4)
+    this.buf.writeInt32LE(v, this.n)
+    this.n += 4
+  }
+
+  writeUInt8(v: number) {
+    this.resize(1)
+    this.buf.writeUInt8(v, this.n)
+    this.n++
+  }
+
+  writeUint8Array(arr: Uint8Array, start: number, len: number) {
+    if (start) {
+      var arrayBuf = arr.buffer // get the underlying ArrayBuffer
+      arr = new Uint8Array(arrayBuf.slice(start, len))
+    }
+    this.resize(arr.length)
+    this.buf.set(arr, this.n)
+    this.n += len
+  }
+
+  slice() {
+    return this.buf.slice(0, this.n)
+  }
+
+  resize(m: number) {
+    var n = this.n
+    if (n + m <= this.buf.length) return
+    var newBuf = Buffer.alloc(nextPow2(n + m))
+    newBuf.set(this.buf.slice(0, n))
+    this.buf = newBuf
+  }
 }
 
-FlexBuffer.prototype.slice = function () {
-  return this.buf.slice(0, this.n)
-}
-
-function resize (self, m) {
-  var n = self.n
-  if (n + m <= self.buf.length) return
-  var newBuf = Buffer.alloc(nextPow2(n + m))
-  newBuf.set(self.buf.slice(0, n))
-  self.buf = newBuf
-}
+export default FlexBuffer
