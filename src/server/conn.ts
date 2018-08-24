03ed87fa5889630e53bf66ccf6731b6396b42436
Eugene Chen <eugenechen@alum.mit.edu>
2018-08-23 17:36:01 -0700

    partially ts server/conn.ts

--- src/server/conn.ts
+++ src/server/conn.ts
@@ -3,103 +3,111 @@ import FlexBuffer from '../protocol/flex-buffer'
 import ChunkIO from '../protocol/chunk-io'
 import config from '../config'
 
-// Creates a new client handle from a new websocket connection
-module.exports = Conn
-
 // Allocate once and re-use
 var buf = new FlexBuffer()
+
+interface Perf {
+  messagesSent: number
+  messagesReceived: number
+  bytesSent: number
+  bytesReceived: number
+}
 
 // Represents one client (player / spectator / hasn't signed in yet / w/e) connected to this game server.
 // Responsibilities:
 // - Track connection state
 // - Track client version
 // - Low-level send and receive messages
-function Conn (ws) {
-  EventEmitter.call(this)
-
+// Creates a new client handle from a new websocket connection
+class Conn extends EventEmitter {
   // A new client just connected, here's the websocket
-  this.ws = ws
-  this.closed = false
-  // Client version. Unknown at first, will be set during the handshake.
-  this.clientVersion = null
-  // Track performance and bandwidth
-  this.perf = {
-    messagesSent: 0,
-    messagesReceived: 0,
-    bytesSent: 0,
-    bytesReceived: 0
+  ws: WebSocket
+  closed: boolean
+  clientVersion: any
+  perf: Perf
+  error: any
+  constructor(ws: WebSocket) {
+    super()
+    this.ws = ws
+    this.closed = false
+    // Client version. Unknown at first, will be set during the handshake.
+    this.clientVersion = null
+    // Track performance and bandwidth
+    this.perf = {
+      messagesSent: 0,
+      messagesReceived: 0,
+      bytesSent: 0,
+      bytesReceived: 0,
+    }
+    this.error = null
+    ws.on('message', handleMessage.bind(this))
+    ws.on('close', handleClose.bind(this))
   }
-  this.error = null
 
-  ws.on('message', handleMessage.bind(this))
-  ws.on('close', handleClose.bind(this))
-}
+  send(message) {
+    if (this.closed) return console.error('Ignoring message, socket closed')
+    if (!(message instanceof Uint8Array)) message = JSON.stringify(message)
+    try {
+      this.ws.send(message)
+      this.perf.messagesSent++
+      this.perf.bytesSent += message.length
+    } catch (e) {
+      console.error('websocket send failed', e)
+    }
+  }
 
-Conn.prototype = Object.create(EventEmitter.prototype)
+  die(error) {
+    if (this.error) return
+    this.error = error
+    this.send({ type: 'error', error: error })
+    setTimeout(this.destroy.bind(this), 1000)
+  }
 
-Conn.prototype.send = function send (message) {
-  if (this.closed) return console.error('Ignoring message, socket closed')
-  if (!(message instanceof Uint8Array)) message = JSON.stringify(message)
-  try {
-    this.ws.send(message)
-    this.perf.messagesSent++
-    this.perf.bytesSent += message.length
-  } catch (e) {
-    console.error('websocket send failed', e)
+  destroy() {
+    this.ws.close()
+  }
+
+  sendHandshake() {
+    this.send({ type: 'handshake', serverVersion: config.SERVER.VERSION })
+  }
+
+  sendObjects(objects) {
+    if (!objects.length) return
+    this.send({
+      type: 'objects',
+      objects: objects,
+    })
+  }
+
+  sendChunks(chunks) {
+    if (!chunks.length) return
+    buf.reset()
+    ChunkIO.write(buf, chunks)
+    this.send(buf.slice())
+  }
+
+  handleClose() {
+    this.closed = true
+    this.emit('close')
+  }
+
+  handleMessage(data) {
+    try {
+      this.perf.messagesReceived++
+      this.perf.bytesReceived += data.length // Approximate. Doesn't count overhead or non-ASCII chars
+      if (typeof data !== 'string') handleBinaryMessage(this, data)
+      else handleJsonMessage(this, JSON.parse(data))
+    } catch (e) {
+      console.error('error handling client message', e)
+    }
   }
 }
 
-Conn.prototype.die = function die (error) {
-  if (this.error) return
-  this.error = error
-  this.send({type: 'error', error: error})
-  setTimeout(this.destroy.bind(this), 1000)
-}
-
-Conn.prototype.destroy = function destroy () {
-  this.ws.close()
-}
-
-Conn.prototype.sendHandshake = function sendHandshake () {
-  this.send({type: 'handshake', serverVersion: config.SERVER.VERSION})
-}
-
-Conn.prototype.sendObjects = function sendObjects (objects) {
-  if (!objects.length) return
-  this.send({
-    type: 'objects',
-    objects: objects
-  })
-}
-
-Conn.prototype.sendChunks = function sendChunks (chunks) {
-  if (!chunks.length) return
-  buf.reset()
-  ChunkIO.write(buf, chunks)
-  this.send(buf.slice())
-}
-
-function handleClose () {
-  this.closed = true
-  this.emit('close')
-}
-
-function handleMessage (data) {
-  try {
-    this.perf.messagesReceived++
-    this.perf.bytesReceived += data.length // Approximate. Doesn't count overhead or non-ASCII chars
-    if (typeof data !== 'string') handleBinaryMessage(this, data)
-    else handleJsonMessage(this, JSON.parse(data))
-  } catch (e) {
-    console.error('error handling client message', e)
-  }
-}
-
-function handleBinaryMessage (conn, data) {
+function handleBinaryMessage(conn: Conn, data) {
   console.error('Ignoring unimplemented binary message, length ' + data.length)
 }
 
-function handleJsonMessage (conn, obj) {
+function handleJsonMessage(conn: Conn, obj) {
   switch (obj.type) {
     case 'handshake':
       return handleHandshake(conn, obj)
@@ -110,6 +118,8 @@ function handleJsonMessage (conn, obj) {
   }
 }
 
-function handleHandshake (conn, obj) {
+function handleHandshake(conn: Conn, obj) {
   conn.clientVersion = obj.clientVersion
 }
+
+export default Conn
