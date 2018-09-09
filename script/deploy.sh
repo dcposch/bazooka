1881b1e786f67023a540b382136efd2a47fcd8de
DC <dcposch@dcpos.ch>
2018-09-09 01:07:27 -0700

    Fix deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,12 @@
-rsync -avr dist static bazooka.city:bazooka/
+echo 'Building...'
+npm run build-server
+npm run build
+
+echo 'Deploying...'
+rsync -avr dist static node_modules bazooka.city:bazooka/
 
 echo 'Killing old server...'
 ssh bazooka.city 'screen -X -S bazooka quit'
+
 echo 'Starting new server...'
 ssh bazooka.city 'cd bazooka && screen -S bazooka -d -m node dist/server'
